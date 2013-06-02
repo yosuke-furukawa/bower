74b44257b6fbaf61a247bc149c5dce13e38c4c24
yosuke-furukawa <yosuke.furukawa@gmail.com>
2013-06-02 13:08:51 +0900

    Show error if unnable to parse bowerrc

--- test/bin.js
+++ test/bin.js
@@ -56,4 +56,33 @@ describe('bin', function () {
     });
   });
 
+  it('should use bowerrc', function (done) {
+    fs.writeFileSync(testDir + '/.bowerrc', '{ "directory":"bower_compo" }');
+    var cp = spawn('node', [__dirname + '/../bin/bower', 'install', 'jquery'], {
+      cwd: testDir
+    });
+
+    cp.on('exit', function (status) {
+      assert.equal(status, 0);
+      assert(fileExists.sync(testDir + '/bower_compo/jquery'));
+      done();
+    });
+  });
+
+  it('should exit with status 1 if unable to parse bowerrc', function (done) {
+    // failed parse JSON
+    fs.writeFileSync(testDir + '/.bowerrc', '{ "directory":"bower_compo", }');
+    var cp = spawn('node', [__dirname + '/../bin/bower', 'install', 'jquery'], {
+      cwd: testDir
+    });
+
+    cp.stderr.on('data', function(data){
+      assert.equal(data.toString(), "bower \u001b[31merror\u001b[39m Unable to parse global .bowerrc file: Unexpected token }\n");
+    });
+    cp.on('exit', function (status) {
+      assert.equal(status, 1);
+      done();
+    });
+  });
+
 });
